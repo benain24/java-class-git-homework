a0a351fc38442037b64dd74bf3c80556227a9073
Benahin Kur <benkur0001@hotmail.com>
2018-02-22 15:27:52 -0700

    added wind engine

---
+++ homeworkModule11_src/engines/WindEngine.java
@@ -0,0 +1,11 @@
+package engines;
+
+import models.GameModel;
+
+public class WindEngine {
+	public void updateState(GameModel state) {
+        double windXVelocity = state.getBall().getxVelocity();
+        state.getBall().setxVelocity(windXVelocity - (.01));
+    }
+	
+}
