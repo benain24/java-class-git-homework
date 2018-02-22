8546709a3e9935ff22737b8fa6ff960ef67fa82b
Benahin Kur <benkur0001@hotmail.com>
2018-02-22 16:02:58 -0700

    adjusted wind speed

--- homeworkModule11_src/engines/WindEngine.java
+++ homeworkModule11_src/engines/WindEngine.java
@@ -5,7 +5,7 @@ import models.GameModel;
 public class WindEngine {
 	public void updateState(GameModel state) {
         double windXVelocity = state.getBall().getxVelocity();
-        state.getBall().setxVelocity(windXVelocity - (.01));
+        state.getBall().setxVelocity(windXVelocity - 0.01);
     }
 	
 }
