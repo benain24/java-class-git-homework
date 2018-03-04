5c53b83c3fdd4c1c9ffa4311a0e46a1c8f02fb9c
Benahin Kur <benkur0001@hotmail.com>
2018-03-03 19:47:55 -0700

    finished Cow class

--- Zoo/Cow.java
+++ Zoo/Cow.java
@@ -2,6 +2,7 @@ public class Cow extends Animal{
 
   private String name;
   private String sound = "Moo...";
+  private int weight = 1400;
 
   Cow(String name){
     super(name);
@@ -11,9 +12,14 @@ public class Cow extends Animal{
     return sound;
   }
 
+  public int getWeight() {
+    return weight;
+  }
+
   public static void main(String args[]){
     Cow beefy = new Cow("beefy");
     System.out.println("Name: " + beefy.getName());
     System.out.println("Sound: " + beefy.getSound());
+    System.out.println("Weight: " + beefy.getWeight() + " pounds");
   }
 }
