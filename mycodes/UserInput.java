bf525ba31a150ac491926ab716b888d35e478e5e
Benahin Kur <benkur0001@hotmail.com>
2018-03-29 20:32:04 -0600

    practicing

---
+++ mycodes/UserInput.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class UserInput
+{
+  public static void main(String[] args) {
+
+    String input;
+
+    System.out.println("What is your first and last name?");
+    Scanner sc = new Scanner(System.in);
+    input = sc.next();
+
+  }
+}
