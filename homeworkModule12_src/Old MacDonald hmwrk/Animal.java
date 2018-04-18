c1427eb0621ee871f25ac5c596133f3b256c495f
Benahin Kur <benkur0001@hotmail.com>
2018-04-17 20:15:46 -0600

    need to create a loop

--- homeworkModule12_src/Old MacDonald hmwrk/Animal.java
+++ homeworkModule12_src/Old MacDonald hmwrk/Animal.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class Animal {
-	
+
 
 	abstract String getName();
 	abstract String getSound();
@@ -8,55 +12,40 @@ public abstract class Animal {
 
 		System.out.println("Old MacDonald had a farm, EIEIO!");
 		System.out.println("And on that farm he had a " + getName() + " EIEIO!");
-		System.out.println("With a " + getSound() + ", " + getSound() + ", here, and a "+ 
+		System.out.println("With a " + getSound() + ", " + getSound() + ", here, and a "+
 			getSound() + ", " + getSound() + " , there,");
-		System.out.println("Here a " + getSound() + ", there a "+ getSound() + ", everywhere a " + 
+		System.out.println("Here a " + getSound() + ", there a "+ getSound() + ", everywhere a " +
 			getSound() + ", "+ getSound() + ",");
 		System.out.println("Old MacDonald had a farm, EIEIO!");
 
 	}
 
+
 	public static void main(String[] args) {
-		
-		Animal [] anim = new Cow();
-	/*	Animal cow = new Cow();
-		Animal dog = new Dog();
+
 		Animal cat = new Cat();
-		Animal horse = new Horse();
-		cow.singVerse();
-		dog.singVerse();
 		cat.singVerse();
+		Animal cow = new Cow();
+		cow.singVerse();
+		Animal dog = new Dog();
+		dog.singVerse();
+		Animal duck = new Duck();
+		duck.singVerse();
+		Animal horse = new Horse();
 		horse.singVerse();
 
-		*/
 	}
+
+/*	public static void main(String[] args) {
+
+		ArrayList<String> anim = new ArrayList<String>();
+		anim.add(new Cat());
+		anim.add(new Cow());
+		anim.add(new Dog());
+		anim.add(new Duck());
+		anim.add(new Horse());
+
+		for (int i = 0; i < anim.size(); i++) {
+		anim.get(i).singVerse();
+	  } */
 }
-/*
-	String name;
-	String sound;
-
-	public Animal(String name, String sound) {
-		this.name = name;
-		this.sound = sound;
-	}
-
-	abstract public void singVerse() {
-		
-	}
-
-		
-		System.out.println("And on that farm he had a " + name + "EIEIO!");
-		System.out.println("With a " + sound + ", " + sound + ", here, and a "+ 
-			sound + ", "+ sound + "there,");
-		System.out.println("Here a " + sound + ", there a "+ sound + ", everywhere a " + 
-			sound + ", "+ sound + ",");
-		System.out.println("Old MacDonald had a farm, EIEIO!");
-	
-	
-}
-
-
-*/
-
-
-
